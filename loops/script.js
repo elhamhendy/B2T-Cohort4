560963af20343c5bc9cd8053a5cab2e9f9996abb
elhamhendy <elham_hendy@yahoo.com>
2018-10-28 13:38:39 -0400

    loops

---
+++ loops/script.js
@@ -0,0 +1,41 @@
+// let num = 1;
+// while (num <=) {
+// 	if (true) {
+
+// 	} else if (true) {
+
+// 	} else if (true) {
+
+// 	} else {
+
+// 	}
+
+
+ let num = 1;
+while (num <=100){ // num <101
+
+		if((num %3 === 0) && (num %5 === 0)) {
+
+		console.log("FizzBuzz")
+		
+	}
+	if (num %3 === 0){
+		console.log("Fizz")
+	
+
+	}else if(num % 5 === 0){
+		console.log("Buzz")
+	
+	
+	 }else {
+	 	console.log(num);
+
+	}
+	num++;
+ }
+
+	
+
+// }
+
+
