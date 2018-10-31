61a2b0c0ea0ff715bdda71520e4b97490b4e521c
elhamhendy <elham_hendy@yahoo.com>
2018-10-30 22:03:08 -0400

    objects h.w

---
+++ library objects/script.js
@@ -0,0 +1,15 @@
+let library = [
+ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true },
+ { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true },
+ { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of TheHunger Games', readingStatus: false }
+ ];
+
+
+for(i = 0; i < library.length; i++){
+
+	if(library[i].readingStatus === true){
+	document.getElementById('display').innerHTML += `already read ${library[i].title} by ${library[i].author} <br>`;
+	} else {
+	document.getElementById('display').innerHTML += `you still need to read ${library[i].title} by ${library[i].author}<br>`;
+	}
+}
