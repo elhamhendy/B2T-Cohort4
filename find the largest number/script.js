5ccd27b274533bdc17224fa98da2ce179ef01ddc
elhamhendy <elham_hendy@yahoo.com>
2018-11-03 19:48:18 -0400

    find the largest no H.W

---
+++ find the largest number/script.js
@@ -0,0 +1,25 @@
+
+		function greaterNumber()
+	{
+	
+		let num1 = (prompt("insert 1st number"));
+		let num2 = (prompt("insert 2nd number"));
+		let num3 = (prompt("insert 3rd number"));
+		
+
+		if(num1 > num2 && num1 > num3){
+			console.log("userNumber1 is greater");
+			
+
+		}else if(num2 > num1 && num2 > num3){
+			console.log("userNumber2 is greater");
+			
+
+		}else{
+			console.log("userNumber3 is greater");
+			
+
+
+		}
+
+	}
